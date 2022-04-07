5898fe3e697216b4cbc3966a543ed054cb2f5017
Parth Bisht <99276868+Parth-Bisht@users.noreply.github.com>
2022-04-07 13:30:03 +0530

    assignment1

--- day1/list/src/App.js
+++ day1/list/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li className='square'>Samsung</li>
+        <li className='square'>HTC</li>
+        <li>Micromax</li>
+        <li className='circle' >Apple</li>
+      </ul>
     </div>
   );
 }
