817c22c02a42abb6577cf81347ca793183f78165
Parth Bisht <99276868+Parth-Bisht@users.noreply.github.com>
2022-04-09 00:31:01 +0530

    assignment

---
+++ day3/payment-card/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import DateLogo from './components/datelogo';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="card">
+      <DateLogo date={"23/02/2021"} img={"https://animationvisarts.com/wp-content/uploads/2020/12/Amazon-Icon-1.jpg"}/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ day3/payment-card/src/components/datelogo.jsx
@@ -0,0 +1,10 @@
+const DateLogo = ({date,img})=>{
+    return(
+        <div>
+            <p>{date}</p>
+            <img src={img}/>
+        </div>
+    )
+}
+
+export default DateLogo
