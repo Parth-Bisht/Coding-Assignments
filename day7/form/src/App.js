d357bbc42ec7f0aa7b31baecc17aea0486da85be
Parth Bisht <99276868+Parth-Bisht@users.noreply.github.com>
2022-04-15 17:18:53 +0530

    commit 2

--- day7/form/src/App.js
+++ day7/form/src/App.js
@@ -5,7 +5,7 @@ import Form from './components/Form';
 function App() {
   return (
     <div className="App">
-    <h1>Form</h1>
+    
      <Form/>
     </div>
   );
--- day7/form/src/components/Form.jsx
+++ day7/form/src/components/Form.jsx
@@ -1,135 +1,157 @@
-import React from 'react'
-import Employee from './Employee';
+import React from "react";
+import Employee from "./Employee";
+import styles from "./Form.module.css";
 
 function Form() {
-    const [employeedata,setemployeedata] = React.useState([]);
-    const [formData,setformData]=React.useState({})
+  const [employeedata, setemployeedata] = React.useState([]);
+  const [formData, setformData] = React.useState({});
+  const [isloading,setisloading] = React.useState(false);
+  const [iserror,setiserror] = React.useState(false);
 
-    const onadd = (newdata)=>{
-        setemployeedata([...employeedata,newdata])
+  const onadd = (newdata) => {
+      setisloading(false)
+    setemployeedata([...employeedata, newdata]);
+  };
+
+  React.useEffect(() => {
+      setisloading(true)
+    getdata();
+  }, []);
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+
+    if (e.target.type === "checkbox") {
+      setformData({
+        ...formData,
+        [inputName]: e.target.checked,
+      });
+    } else {
+      setformData({
+        ...formData,
+        [inputName]: e.target.value,
+      });
     }
+  };
 
-    React.useEffect(()=>{
-        getdata();
-    },[])
-
-    const handleChange = (e)=>{
-        const inputName = (e.target.name);
-
-        if (e.target.type === "checkbox") {
-            setformData({
-              ...formData,
-              [inputName]: e.target.checked,
-            });
-        }
-        else {
-            setformData({ 
-                ...formData,
-                [inputName]:e.target.value
-            })
-        }
-        
-    }
-
-    const getdata = ()=>{
-        fetch(`http://localhost:3000/data`)
-        .then((res) => res.json())
-        .then((data)=> setemployeedata(data))
-        .catch((e)=>{
-            console.log(e)
+  const getdata = () => {
+    fetch(`http://localhost:3000/data`)
+      .then((res) => res.json())
+      .then((data) => {
+          setemployeedata(data);
+          setisloading(false)
         })
-    }
+      .catch((e) => {
+        setisloading(false)
+          setiserror(true);
+        console.log(e);
+      });
+  };
 
-
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        senddata(formData);       
-    }
-    const senddata = async (value)=>{
-        let res = await fetch(`http://localhost:3000/data`,{
-            method:"POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(value)
-        })
-        let data = await res.json();
-        onadd(data);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    senddata(formData);
+  };
+  const senddata = async (value) => {
+      setisloading(true)
+    let res = await fetch(`http://localhost:3000/data`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(value),
+    });
+    let data = await res.json();
+    onadd(data);
+  };
   return (
-      <>
-    <div>
-      <form onSubmit={handleSubmit}>
-          <label >
-              Name:
+    <>
+      <div className={styles.border}>
+        <h1 style={{ textAlign: "center" }}>Form</h1>
+        <form className={styles.mainForm} onSubmit={handleSubmit}>
+          <label>
+            <strong> Name: </strong>
           </label>
-          <input onChange={handleChange} type="text" name="name" placeholder="Enter your name" />
-          <br />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+          />
 
-          <label >
-              Age:
+          <label>
+            <strong> Age:</strong>
           </label>
-          <input onChange={handleChange} type="number" name="age" placeholder="Enter your age" />
-          <br />
+          <input
+            onChange={handleChange}
+            type="number"
+            name="age"
+            placeholder="Enter your age"
+          />
 
-          <label >
-              Address:
+          <label>
+            <strong> Address: </strong>
           </label>
-          <input onChange={handleChange} type="text" name="address" placeholder="Enter your address" />
-          <br />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="address"
+            placeholder="Enter your address"
+          />
 
-          <label >
-              Department:
+          <label>
+            <strong> Department: </strong>
           </label>
           <select onChange={handleChange} name="department">
-              <option value="--">--</option>
-              <option value="IT">IT</option>
-              <option value="Production">Production</option>
-              <option value="Marketing ">Marketing </option>
-              <option value="Finance  ">Finance  </option>
-              <option value="Sales  ">Sales  </option>
+            <option value="--">--</option>
+            <option value="IT">IT</option>
+            <option value="Production">Production</option>
+            <option value="Marketing ">Marketing </option>
+            <option value="Finance  ">Finance </option>
+            <option value="Sales  ">Sales </option>
           </select>
-          <br />
 
-          <label >
-              Salary:
+          <label>
+            <strong> Salary: </strong>
           </label>
-          <input onChange={handleChange} type="number" name="salary" placeholder="Enter your salary" />
-          <br />
+          <input
+            onChange={handleChange}
+            type="number"
+            name="salary"
+            placeholder="Enter your salary"
+          />
 
-            <label >
-                Married:
-            </label>
-            <input type="checkbox" name="married" onChange={handleChange}/>
-          <br />
-
-          <input type="submit" />
-      </form>
-    </div>
-    <div>
-    <table border="1">
-    <thead>
-    <tr>
-            <th>ID</th>
-            <th>NAME</th>
-            <th>ADDRESS</th>
-            <th>AGE</th>
-            <th>DEPARTMENT</th>
-            <th>SALARY</th>
-            <th>MARRIED</th>
-        </tr>
-    </thead>
-    <tbody>   
-    {employeedata.map((item)=>{
-            return <Employee key={item.id} data={item}/>
-        })}
-    </tbody>
-     
-    </table>
-        
-    </div>
+          <label>
+            <strong> Married: </strong>
+          </label>
+          <input type="checkbox" name="married" onChange={handleChange} />
+          <span></span>
+          <input className={styles.btn} type="submit" />
+        </form>
+      </div>
+      <div>
+      {isloading? (<div>...loading</div>) : iserror? (<div>Something went wrong</div>) : ( <table border="1">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>ADDRESS</th>
+              <th>AGE</th>
+              <th>DEPARTMENT</th>
+              <th>SALARY</th>
+              <th>MARRIED</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employeedata.map((item) => {
+              return <Employee key={item.id} data={item} />;
+            })}
+          </tbody>
+        </table>)}
+       
+      </div>
     </>
-  )
+  );
 }
 
-export default Form
+export default Form;
